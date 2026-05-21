f960d0b0b4c96010c581748fffab7faf4838af04
takaya-47 <local.s.s.47@gmail.com>
2026-05-21 10:17:47 +0900

    [v1.0.1]Go Doc追加

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/takaya-47/learn-go-ex10
+
+go 1.26.3
--- solution.go
+++ solution.go
@@ -1,5 +1,10 @@
+// Package ch10_solution contains the solution to the exercises in chapter 10 of Learning Go, 2nd edition.
 package solution
 
+// Add is a function that takes two integers and returns their sum.
+// It follows the rules specified by [mathsisfun]
+//
+// [mathsisfun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
