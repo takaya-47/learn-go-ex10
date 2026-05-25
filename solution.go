17dac47d411c57662056574b12afc28f5781b30a
takaya-47 <local.s.s.47@gmail.com>
2026-05-25 09:08:02 +0900

    Addインターフェースのメジャーアップデート

--- solution.go
+++ solution.go
@@ -1,10 +1,16 @@
 // Package solution は「初めてのGo 第2版」の10章に関するパッケージです。
 package solution
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add は2つの整数を足し算して、その結果を返します。
 // 足し算については[mathsisfun]のサイトを参考にしてください。
 //
 // [mathsisfun]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
